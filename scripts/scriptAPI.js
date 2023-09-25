6e8ec492ca42b12192f8acd2c74fd1833814f2bb
Lisa <lisa.margo@etu.unicaen.fr>
2023-09-25 14:45:13 +0200

    post code and city input

--- scripts/scriptAPI.js
+++ scripts/scriptAPI.js
@@ -1,10 +1,11 @@
 const postCodeBase = 'https://geo.api.gouv.fr/communes?codePostal=';
-const codeInput = document.getElementById("codeInput");
+const codeInput = document.getElementById("postCodeInput");
+const cityRow = document.getElementById("cityRow");
 
 let codeInputValue = codeInput;
 
 
-document.getElementById("inputForm").addEventListener("submit", function(event) {
+document.getElementById("postCodeForm").addEventListener("submit", function(event) {
     event.preventDefault();
     fetch(postCodeBase+codeInput.value)
         .then(response => {
@@ -15,11 +16,22 @@ document.getElementById("inputForm").addEventListener("submit", function(event) 
         })
         .then(data => {
             console.log(data);
+            data.forEach(element => {
+                let row = document.getElementById("cityInput");
+                const option = document.createElement("option");
+                option.text = element.nom;
+                option.value = element.code;
+                row.add(option);
+            });
         })
         .catch(error => {
             console.log('There was a problem with the fetch operation : ', error);
         });
 });
+
+decodeURIComponent.getElementById("cityRow").addEventListener("submit", function(event) {
+    event.preventDefault();
+})
 
 
 /*fetch('https://api.meteo-concept.com/api/ephemeride/0?insee=35238&token=7fa72ed46c441a1e1b051d2f92ab251fb9d631106607663f600e8325af307829')
